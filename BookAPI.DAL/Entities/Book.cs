e436b134a3c4b2c55ffeea80e732f257ec7a4c69
MüzeyyenSanioğlu <MzeyyenSaniolu@MUZEYYEN>
2022-06-10 10:20:31 +0300

    Add project files.

---
+++ BookAPI.DAL/Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAPI.DAL.Entities
+{
+    public class Book
+    {
+        public string UUID { get; set; }
+        public string Title { get; set; }
+
+    }
+}
